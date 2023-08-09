a1a91840c742c1c9766f3b2c11deb6223cf7cfc2
Михаил Денисов <Migelius.invest@gmail.com>
2023-08-09 17:20:04 +0300

    Fixed3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,33 +5,18 @@ public class Player : MonoBehaviour
 {
     private int _maxHealth = 100;
     private float _health;
-
-    public static Action<int> HealthChenged;
+    public Action<int> HealthChenged;
 
     private void Awake()
     {
         _health = _maxHealth;
     }
 
-    public void Heal()
-    {
-        int healPoint = 10;
-
-        ChengeHealth(healPoint);
-    }
-
-    public void TakeDamage()
-    {
-        int damagePoint = -10;
-
-        ChengeHealth(damagePoint);
-    }
-
-    private void ChengeHealth(int value)
+    public void ChengeHealth(int value)
     {
         int minHealth = 0;
         _health += Mathf.Clamp(value, minHealth, _maxHealth);
 
-        HealthChenged.Invoke(value);
+        HealthChenged?.Invoke(value);
     }
 }
